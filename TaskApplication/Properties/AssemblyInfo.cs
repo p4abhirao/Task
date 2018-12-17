b65e30cec349f2b87d3807442f3b746489a4f244
abhishek <p4abhirao@gmail.com>
2018-12-17 16:46:50 +0530

    Application

---
+++ TaskApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TaskApplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TaskApplication")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c26ce7c6-dcf9-441d-8313-8da2dbdc5377")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TaskApplication/Scripts/custom.js
@@ -0,0 +1,43 @@
+﻿
+
+function DataSave() {
+    var vartxtName = "", vartxtDesc = "", vartxtCost = "", varddlPrioritylist = "";
+    vartxtName = $("#txtName").val();
+    vartxtDesc = $("#txtDesc").val();
+    vartxtCost = $("#txtCost").val();
+    varddlPrioritylist = $("#ddlPrioritylist").val();
+
+    var sUrl = '<%=System.Configuration.ConfigurationManager.AppSettings["APIUrl"].ToString() %>';
+
+    model = {
+        Name: vartxtName,
+        Description: vartxtDesc,
+        EstimatedCost: vartxtCost,
+        PriorityID: varddlPrioritylist
+    };
+    ClearControl();
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        //dataType: 'json',
+        data: JSON.stringify(model),
+        url: sUrl + "/api/Task/TaskSave",
+        async: false,
+        success: function (data) {
+            alert("Data Save Successfully.");
+        },
+        complete: function () {
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+
+    });
+}
+
+function ClearControl() {
+    $('input#txtName').val(''),
+    $('input#txtDesc').val('');
+    $('input#txtCost').val('');
+    $("#ddlPrioritylist").val("Select");
+}
