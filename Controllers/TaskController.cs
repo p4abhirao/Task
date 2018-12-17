925131a110fdf63f9c297147efcf2b71dc2fb5fc
abhishek <p4abhirao@gmail.com>
2018-12-17 15:40:15 +0530

    Initial commit

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Runtime.Serialization.Json;
+using TaskApplication.Models;
+
+namespace TaskApplication.Controllers
+{
+	public class TaskController : Controller
+	{
+		// GET: Task
+		public ActionResult Index()
+		{
+
+			return View();
+		}
+
+		public ActionResult TaskCreate()
+		{
+			try
+			{
+				List<SelectListItem> PriorityList = new List<SelectListItem>();
+
+				PriorityList.Add(new SelectListItem { Text = "P1", Value = "1" });
+				PriorityList.Add(new SelectListItem { Text = "P2", Value = "2" });
+				PriorityList.Add(new SelectListItem { Text = "P3", Value = "3" });
+				ViewData["PriorityList"] = (IEnumerable<SelectListItem>)PriorityList;
+			}
+			catch (Exception ex)
+			{
+
+				throw ex;
+			}
+			return View();
+		}
+
+        /// <summary>
+        /// Get Task list
+        /// </summary>
+        /// <returns></returns>
+		public ActionResult TaskList()
+		{
+            string result = string.Empty, uri = "", sMethod = "POST";
+            uri = System.Configuration.ConfigurationManager.AppSettings["APIUrl"] + "api/Task/TaskList?sText=0";
+            var lTask = new List<Task>();
+
+            //request object
+            HttpWebRequest req = WebRequest.Create(uri) as HttpWebRequest;
+            req.KeepAlive = false;
+            req.Method = sMethod;
+            req.ContentType = "application/json";
+
+           
+                HttpWebResponse httpResponse = (HttpWebResponse)req.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    // store response body
+                    result = streamReader.ReadToEnd();
+
+                    using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(result)))
+                    {
+                        //converting json to class
+                        var serializer = new DataContractJsonSerializer(lTask.GetType());
+                        lTask = serializer.ReadObject(stream) as List<Task>;
+
+                    }
+
+                    httpResponse.Close();
+                }
+                
+			return View(lTask);
+		}
+	}
+}
---
+++ Models/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskApplication.Models
+{
+	public class Task
+	{
+		public int TaskId { get; set; }
+		[Display(Name = "Name")]
+		public string Name { get; set; }
+		[Display(Name = "Description")]
+		public string Description { get; set; }
+		[Display(Name = "Date")]
+		public DateTime TaskDate { get; set; }
+		[Display(Name = "Cost")]
+		public decimal EstimatedCost { get; set; }
+		public int PriorityID { get; set; }
+		[Display(Name = "Priority")]
+		public string PriorityName { get; set; }
+	}
+
+	public enum ePriorityType : int
+	{
+		P1 = 1,
+		P2 = 2,
+		P3 = 3,
+	}
+}
